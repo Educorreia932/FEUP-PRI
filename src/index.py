993f05ff57e175c131db4289deb43e7fecee5584
Eduardo Correia <skelozard1@gmail.com>
2021-10-28 16:51:08 +0100

    Moved Spotify credentials to a .env file

--- src/index.py
+++ src/index.py
@@ -1,16 +1,14 @@
 import json
 import spotipy
 
+from dotenv import load_dotenv
 from rich import print
 from spotipy.oauth2 import SpotifyOAuth
 
-SPOTIPY_CLIENT_ID="511e04ae8586475cbf6b4b0914228bee"
-SPOTIPY_CLIENT_SECRET="4d9bcb0f2ad548928b345c05a08f286b"
+load_dotenv() 
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri="http://127.0.0.1:8000/redirect",
-    client_id=SPOTIPY_CLIENT_ID,
-    client_secret=SPOTIPY_CLIENT_SECRET
 ))
 
 f = open("../dataset/data/mpd.slice.0-999.json")
