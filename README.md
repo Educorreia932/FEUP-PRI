8d0e2e1746768c44024910b214dbaad769e30f20
Eduardo Correia <skelozard1@gmail.com>
2021-10-29 00:31:27 +0100

    Added Peewee as a database ORM and created classes for the tables
    - Created a requirements.txt file to list the necessary Python libraries
    - Added instructions on how to setup the project in the README.md

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Information Retrieval and Processing
 
 ![Database Scheme](out/database/database.png)
+
+## Instructions
+
+- Install Python modules by running the following command:
+
+```sh
+pip install -r requirements.txt
+```
+
+- Create a `.env` file in the `src` folder, containing the Spotify application credentials:
+
+```
+SPOTIPY_CLIENT_ID=<client_id>
+SPOTIPY_CLIENT_SECRET=<client_secret>
+```
+
+- Run the Python file:
+
+```sh
+cd src
+python index.py 
+```
--- src/index.py
+++ src/index.py
@@ -2,11 +2,71 @@ import json
 import spotipy
 
 from dotenv import load_dotenv
+from peewee import *
 from rich import print
 from spotipy.oauth2 import SpotifyOAuth
 
+# Setup database
+
+db = SqliteDatabase("database.db")
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class AlbumType(IntegerField):
+    def __init__(self):
+        choices = [
+            (1, "album"),
+            (2, "single"),
+            (3, "compilation")
+        ]
+
+        self.to_db = {v: k for k, v in choices}
+        self.from_db = {k: v for k, v in choices}
+        super(IntegerField, self).__init__()
+
+
+class Album(BaseModel):
+    name = CharField()
+    uri = CharField()
+    album_type = AlbumType()
+
+
+class Lyrics(BaseModel):
+    text = CharField()
+
+
+class Track(BaseModel):
+    duration_ms = IntegerField()
+    lyrics = ForeignKeyField(Lyrics)
+
+
+class TrackPosition(Model):
+    album = ForeignKeyField(Album)
+    track = ForeignKeyField(Track)
+
+    class Meta:
+        primary_key = CompositeKey('album', 'track')
+
+
+class Artist(BaseModel):
+    name = CharField()
+
+
+class Genre(BaseModel):
+    name = CharField()
+
+
+db.connect()
+db.create_tables([Album, Track, Lyrics, Artist, Genre])
+
+# Load environment variables
 load_dotenv() 
 
+# Create Spotify connection
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     redirect_uri="http://127.0.0.1:8000/redirect",
 ))
