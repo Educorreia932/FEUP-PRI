4a3aecc4f1b9a7b5dd16c41b0361059f89cd0fbe
Eduardo Correia <skelozard1@gmail.com>
2021-10-29 10:07:51 +0100

    Added missing fields for some tables
    - Started to create objects in the database
    - Deleted unused SQL file

--- src/index.py
+++ src/index.py
@@ -29,10 +29,16 @@ class AlbumType(IntegerField):
         super(IntegerField, self).__init__()
 
 
-class Album(BaseModel):
+class Genre(BaseModel):
     name = CharField()
-    uri = CharField()
+
+
+class Album(BaseModel):
+    uri = CharField(unique=True)
+    name = CharField()
     album_type = AlbumType()
+    release_date = DateField()
+    total_tracks = IntegerField()
 
 
 class Lyrics(BaseModel):
@@ -40,31 +46,44 @@ class Lyrics(BaseModel):
 
 
 class Track(BaseModel):
+    uri = CharField(unique=True)
     duration_ms = IntegerField()
     lyrics = ForeignKeyField(Lyrics)
+    popularity = IntegerField()
+    acousticness = FloatField()
+    danceability = FloatField()
+    energy = FloatField()
+    instrumentalness = FloatField()
+    liveness = FloatField()
+    loudness = FloatField()
+    mode = IntegerField()
+    speechiness = FloatField()
+    tempo = FloatField()
+    time_signature = IntegerField()
+    valence = FloatField()
 
 
-class TrackPosition(Model):
+class TrackNumber(BaseModel):
     album = ForeignKeyField(Album)
     track = ForeignKeyField(Track)
+    number = IntegerField()
 
     class Meta:
-        primary_key = CompositeKey('album', 'track')
+        primary_key = CompositeKey("album", "track")
 
 
 class Artist(BaseModel):
+    uri = CharField(unique=True)
     name = CharField()
-
-
-class Genre(BaseModel):
-    name = CharField()
+    genres = ManyToManyField(Genre)
+    popularity = IntegerField()
 
 
 db.connect()
-db.create_tables([Album, Track, Lyrics, Artist, Genre])
+db.create_tables([Album, Artist, Genre, Lyrics, Track, TrackNumber])
 
 # Load environment variables
-load_dotenv() 
+load_dotenv()
 
 # Create Spotify connection
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
@@ -76,5 +95,15 @@ f = open("../dataset/data/mpd.slice.0-999.json")
 playlists = json.load(f)["playlists"]
 
 track = playlists[0]["tracks"][0]
+track_uri = track["track_uri"].split(":")[2]
 
-print(sp.track(track["track_uri"].split(":")[2]))
+track_data = sp.track(track_uri)
+album = track_data["album"]
+
+Album.create(
+    uri=album["uri"],
+    name=album["name"],
+    album_type=album["album_type"],
+    release_date=album["release_date"],
+    total_tracks=album["total_tracks"]
+)
