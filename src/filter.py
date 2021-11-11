7cc85bd3e613c3a168e4c04934ca81e7dbd5838f
joaoalc joaoalc <up201806531@fe.up.pt>
2021-11-11 19:30:21 +0000

    Database filtering completed

--- src/filter.py
+++ src/filter.py
@@ -7,6 +7,19 @@ import sqlalchemy_utils as sqla_utils
 from rich import print
 from better_profanity import profanity
 
+
+def has_swear_word(text):
+    if text == None:
+        return False
+    words = text.split()
+    for w in words:
+        for sw in swear_words:
+            if sw.lower() == w.lower():
+                return True
+    return False
+
+
+#Connect to database
 con = sqlite3.connect(os.path.join(os.path.dirname(__file__), "../data/database.db"))
 
 album = pd.read_sql_query("SELECT * from album", con)
@@ -19,7 +32,7 @@ track = pd.read_sql_query("SELECT * from track", con)
 track_artist_through = pd.read_sql_query("SELECT * from track_artist_through", con)
 #close the connection
 con.close()
-"""
+
 #Check null values
 print("Check if there are null values")
 print("Album")
@@ -36,55 +49,42 @@ print("Genre")
 print(genre.isnull().any(axis=1).any())
 print("Track")
 print(track.isnull().any(axis=1).any())
+
 print("TrackArtist")
 print(track_artist_through.isnull().any(axis=1).any())
 
-
-
-#Find edge cases
-print("Finding edge cases")
-print("Empty string names")
-print("Track")
-print(track.loc[track['name'] == ""])
-#There weren't any
-#print("Artist")
-#print(artist.loc[artist['name'] == ""])
-print("Album")
-print(album.loc[album['name'] == ""])
-#There weren't any
-#print("Genre")
-#print(genre.loc[genre['name'] == ""])
-"""
-
 #Flag innappropriate content
 
+#Get swear word list
+with open(os.path.join(os.path.dirname(__file__), "../src/swear_word_list.txt"), 'r') as f:
+    swear_words = [line.strip() for line in f]
+
+
 #Artist
-artist['explicit'] = artist['name'].apply(lambda x: profanity.contains_profanity(x))
-print("Number of censored artist names:")
-print(artist['explicit'].value_counts(normalize=False))
+artist['explicit'] = artist['name'].apply(lambda x: has_swear_word(x))
+#print("Number of censored artist names:")
+#print(artist['explicit'].value_counts(normalize=False))
 
 #Track
-track['explicitname'] = track['name'].apply(lambda x: profanity.contains_profanity(x))
-print("Number of censored track names:")
-print(track['explicitname'].value_counts(normalize=False))
+track['explicitname'] = track['name'].apply(lambda x: has_swear_word(x))
+#print("Number of censored track names:")
+#print(track['explicitname'].value_counts(normalize=False))
 
-track['explicittrack'] = track['lyrics'].apply(lambda x: profanity.contains_profanity(x))
-print("Number of censored track lyrics:")
-print(track['explicittrack'].value_counts(normalize=False))
+track['explicittrack'] = track['lyrics'].apply(lambda x: has_swear_word(x))
+#print("Number of censored track lyrics:")
+#print(track['explicittrack'].value_counts(normalize=False))
 
 track['explicit'] = track['explicitname'] | track['explicittrack']
 #print("Number of censored track lyrics or names:")
-print(track['explicit'].value_counts(normalize=False))
+#print(track['explicit'].value_counts(normalize=False))
 
-del track['explicittrack']
-del track['explicitname']
-
+track.drop(columns=['explicittrack', 'explicitname'], inplace=True)
 
 
 #Album
-album['explicit'] = album['name'].apply(lambda x: profanity.contains_profanity(x))
+album['explicit'] = album['name'].apply(lambda x: has_swear_word(x))
 #print("Number of censored album names:")
-print(album['explicit'].value_counts(normalize=False))
+#print(album['explicit'].value_counts(normalize=False))
 
 #Save into new database
 engine = sqla.create_engine("sqlite:///" +  os.path.join(os.path.dirname(__file__), "../data/database_filtered.db"))
@@ -92,15 +92,24 @@ engine = sqla.create_engine("sqlite:///" +  os.path.join(os.path.dirname(__file_
 if not sqla_utils.database_exists(engine.url):
     sqla_utils.create_database(engine.url)
 
-print(track)
 
-album.to_sql('album', engine, if_exists="replace", index=False)
-album_artist_through.to_sql('album_artist_through', engine, if_exists="replace", index=False)
-albumtrack.to_sql('albumtrack', engine, if_exists="replace", index=False)
-artist.to_sql('artist', engine, if_exists="replace", index=False)
-album_artist_through.to_sql('artist_genre_through', engine, if_exists="replace", index=False)
-genre.to_sql('genre', engine, if_exists="replace", index=False)
-track.to_sql('track', engine, if_exists="replace", index=False)
-track_artist_through.to_sql('track_artist_through', engine, if_exists="replace", index=False)
+with engine.begin() as connection:
+    album.to_sql('album', engine, if_exists="replace", index=False)
+    album_artist_through.to_sql('album_artist_through', engine, if_exists="replace", index=False)
+    albumtrack.to_sql('albumtrack', engine, if_exists="replace", index=False)
+    artist.to_sql('artist', engine, if_exists="replace", index=False)
+    album_artist_through.to_sql('artist_genre_through', engine, if_exists="replace", index=False)
+    genre.to_sql('genre', engine, if_exists="replace", index=False)
+    track.to_sql('track', engine, if_exists="replace", index=False)
+    track_artist_through.to_sql('track_artist_through', engine, if_exists="replace", index=False)
+    engine.execute("PRAGMA foreign_keys = ON")
+    engine.execute("DELETE FROM track WHERE name == \"\" OR name IS NULL;")
+    engine.execute("DELETE FROM track WHERE lower(name) LIKE '%instrumental%';")
+    engine.execute("DELETE FROM album WHERE name == \"\" OR name IS NULL;")
+    engine.execute("DELETE FROM album WHERE lower(name) LIKE '%instrumental%';")
+    engine.execute("DELETE FROM artist WHERE name == \"\" OR name IS NULL;")
+    engine.execute("DELETE FROM genre WHERE name == \"\" OR name IS NULL;")
+    
+
 
 engine.dispose()
