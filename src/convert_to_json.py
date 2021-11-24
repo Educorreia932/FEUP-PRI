7fd07ebf5151ec76d9eed25ced37676f0237cb5b
Eduardo Correia <skelozard1@gmail.com>
2021-11-24 15:56:54 +0000

    Adding artist genres to JSON object

--- src/convert_to_json.py
+++ src/convert_to_json.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 db.init(os.path.dirname(__file__) + "/../data/database.db")
 
 TrackArtist = Track.artists.get_through_model()
+ArtistGenre = Artist.genres.get_through_model()
 
 tracks = Track.select().dicts().execute()
 
@@ -20,8 +21,25 @@ for track in tqdm(tracks):
     track_artists = TrackArtist.select().where(TrackArtist.track_id == track["id"]).dicts().execute()
 
     for track_artist in track_artists:
-        artist = model_to_dict(Artist.get(track_artist["id"]))
+        artist = model_to_dict(Artist.get(track_artist["artist"]))
+
+        track_artist_genres = ArtistGenre.select().where(ArtistGenre.artist_id == track_artist["id"]).dicts().execute()
+
+        genres = []
+
+        # Iterate over artist genres
+        for track_artist_genre in track_artist_genres:
+            genre = model_to_dict(Genre.get(track_artist_genre["genre"]))
+
+            genres.append(genre["name"])
+
+        artist["genres"] = genres
 
         artists.append(artist)
 
+        print(artist)
+
     track["artists"] = artists
+
+    break 
+
