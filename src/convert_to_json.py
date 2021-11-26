9509e7b91bb5faf91e9542900d11596067f6411d
Eduardo Correia <skelozard1@gmail.com>
2021-11-26 00:56:16 +0000

    Adding album artists to JSON object and exporting it
    Also created a serializal method for dates

--- src/convert_to_json.py
+++ src/convert_to_json.py
@@ -1,18 +1,28 @@
+import json
 import os
 
+from datetime import datetime, date
 from models import *
 from rich import print
 from playhouse.shortcuts import model_to_dict
 from tqdm import tqdm
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
 
 # Setup database
 db.init(os.path.dirname(__file__) + "/../data/database.db")
 
 TrackArtist = Track.artists.get_through_model()
 TrackAlbum = Track.albums.get_through_model()
+AlbumArtist = Album.artists.get_through_model()
 ArtistGenre = Artist.genres.get_through_model()
 
-tracks = Track.select().dicts().execute()
+tracks = list(Track.select().dicts().execute())
 
 # Iterate over tracks
 for track in tqdm(tracks):
@@ -25,29 +35,39 @@ for track in tqdm(tracks):
     for track_artist in track_artists:
         artist = model_to_dict(Artist.get(track_artist["artist"]))
 
-        track_artist_genres = ArtistGenre.select().where(ArtistGenre.artist_id == track_artist["id"]).dicts().execute()
+        track_artist_genres = list(ArtistGenre.select().where(ArtistGenre.artist_id == track_artist["id"]).dicts().execute())
 
         genres = []
 
         # Iterate over artist genres
         for track_artist_genre in track_artist_genres:
-            genre = model_to_dict(Genre.get(track_artist_genre["genre"]))
-
-            genres.append(genre["name"])
+            genres.append(model_to_dict(Genre.get(track_artist_genre["genre"])))
 
         artist["genres"] = genres
 
         artists.append(artist)
 
     # Iterate over track albums
-    track_albums = TrackAlbum.select().where(TrackAlbum.track_id == track["id"]).dicts().execute()
+    track_albums = list(TrackAlbum.select().where(TrackAlbum.track_id == track["id"]).dicts().execute())
 
     for track_album in track_albums:
         album = model_to_dict(Album.get(track_album["album"]))
+
+        album_artists = list(AlbumArtist.select().where(AlbumArtist.album_id == track_album["id"]).dicts().execute())
+
+        artists = []
+
+        # Iterate over album artists
+        for album_artist in album_artists:
+            artists.append(model_to_dict(Artist.get(album_artist["artist"])))
+
+        album["artists"] = artists
 
         albums.append(album)
 
     track["artists"] = artists
     track["albums"] = albums
 
-    break 
+# Save file
+with open(os.path.dirname(__file__) + "/../data/database.json", "w") as json_file:
+    json.dump(tracks, json_file, default=json_serial)
