2a346c712b7f38b6443befa4bb0388e7a7c45fa6
joaoalc joaoalc <up201806531@fe.up.pt>
2021-12-07 16:34:51 +0000

    Added wip changes

--- Information Retrieval/startup.sh
+++ Information Retrieval/startup.sh
@@ -5,6 +5,11 @@ precreate-core music
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/schema.json \
+    http://localhost:8983/solr/music/schema
+
+
 # Populate collection
 bin/post -c music /data/database.json
 
