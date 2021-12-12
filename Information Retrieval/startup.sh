40b1905041bce203e5f2a82b3d7353f5d8aa91f6
joaoalc joaoalc <up201806531@fe.up.pt>
2021-12-12 12:08:42 +0000

    .

--- Information Retrieval/startup.sh
+++ Information Retrieval/startup.sh
@@ -2,16 +2,24 @@
 
 precreate-core music
 
-# Start Solr in background mode so we can use the API to upload the schema
-solr start
 
+
+# Start Solr in background mode so we can use the API to upload the schema
+echo "\n--- solr start"
+solr start
+#echo "\n--- sleep 5"
+#sleep 5
+echo "\n--- send schema"
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/music/schema
-
-
+#echo "\n--- sleep 1"
+#sleep 1
+echo "\n--- send db"
 # Populate collection
 bin/post -c music /data/database.json
-
+#echo "\n--- sleep 1"
+#sleep 1
+echo "\n--- solr restart"
 # Restart in foreground mode so we can access the interface
 solr restart -f
