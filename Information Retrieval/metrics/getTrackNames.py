d60d57f4fb9c1c798b70116419d357b7772207c4
joaoalc joaoalc <up201806531@fe.up.pt>
2022-01-17 23:22:07 +0000

    Added statistic stuff

--- Information Retrieval/metrics/getTrackNames.py
+++ Information Retrieval/metrics/getTrackNames.py
@@ -6,12 +6,14 @@ import json
 import requests
 import pandas as pd
 
-QUERY_URL = "http://localhost:8983/solr/music/query?q=live&q.op=OR&defType=edismax&indent=true&qf=name%5E3%20lyrics%5E5%20artists.name%20artists.genres%20albums.name"
+QUERY_URL = "http://localhost:8983/solr/music/query?q=love&q.op=OR&defType=edismax&indent=true&qf=name%20lyrics%20artists.name%20artists.genres%20albums.name"
+QUERY = 4
+WEIGHT = 0
 
 results = requests.get(QUERY_URL).json()['response']['docs']
 
 
-with open('names.txt','w') as tf:
+with open('search_names_and_jsons/names_w' + str(WEIGHT) + '_q' + str(QUERY) + '.txt','w') as tf:
     for r in results:
         tf.write(r["name"])
         tf.write("\n")
